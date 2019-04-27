ba9aa025dfa8b44ec8bcb378a954771fddd0b12a
Kseniya Nahurnaya <kseniya.nahurnaya@gmail.com>
2019-04-27 14:26:01 +0200

    7

--- 1-javascript/js/7-objects.js
+++ 1-javascript/js/7-objects.js
@@ -8,22 +8,22 @@ let kaja = {
     }
 }
 
-let krystian = {
-    imie: "Krystian",
-    wzrost: 182,
-    przedstawOsobe() {
-        console.log(this.imie);
-    }
-}
+// let krystian = {
+//     imie: "Krystian",
+//     wzrost: 182,
+//     przedstawOsobe() {
+//         console.log(this.imie);
+//     }
+// }
 
-kaja.przedstawOsobe();
-krystian.przedstawOsobe();
+// kaja.przedstawOsobe();
+// krystian.przedstawOsobe();
 
-console.log(kaja['wzrost']);
+// console.log(kaja['wzrost']);
 
-kaja.wzrost = 200;
+// kaja.wzrost = 200;
 
-console.log(kaja);
+// console.log(kaja);
 
 let moneataZl = {
     promien: 20,
@@ -34,3 +34,26 @@ let moneataZl = {
 }
 
 console.log(moneataZl.material);
+
+class Osoba {
+    constructor(imie, nazwisko, wzrost, oczy) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wzrost = wzrost;
+        this.oczy = oczy;
+    }
+
+
+
+wyswietlInfo () {
+    console.log("Imię:" + this.imie + "/n"+
+    "Nazwisko:" + this.nazwisko + "/n" +
+    "Wzrost: " + this.wzrost + "/n" +
+    "Oczy: "+ this.oczy);
+}
+}
+let krystian = new Osoba('Krystian', 'Dziopa', 180, 'niebieskie');
+console.log(krystian);
+
+let andrzej = new Osoba('Andrzej', 'Ktoś', 179, 'niebieskie');
+console.log(andrzej);
